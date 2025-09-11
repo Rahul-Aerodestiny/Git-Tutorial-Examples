095a9f5823b5b5e5fd7ae36929358500290760a5
Rahul <pylearn26@gmail.com>
2025-09-11 09:55:12 +0530

    Enhancement to python file

--- example_5.py
+++ example_5.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+# This is an enhancemnet to python code.
+
+def print_name(name):
+    print(f"Hello {name}!")
+    
+def main():
+    # This is the entry point for our programm
+    name = input("Enter Your Name:\n")
+    print_name(name)
+    
+main()
