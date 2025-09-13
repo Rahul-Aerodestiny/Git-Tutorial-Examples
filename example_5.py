d4358c5896d640a928a0ec9ef76e2f7be846173b
Rahul <pylearn26@gmail.com>
2025-09-13 13:41:48 +0530

    Add greet user feature

--- example_5.py
+++ example_5.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+def greet_user(name):
+    print(f"Hello {name}!")
+
+def main():
+    user_name = input("What's your name?")
+    greet_user(user_name)
+    
+if __name__ == "__main__":
+    main()
